2b9e3d44d6055126e32313ff0e2ec1eb633a3577
Mars1989sh <mars@same.com>
2017-08-27 16:00:58 +0800

    creat project

---
+++ VoiceMemo/VoiceMemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VoiceMemo
+//
+//  Created by Mars Zhang on 2017/8/27.
+//  Copyright © 2017年 Mars Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
